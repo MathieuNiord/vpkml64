62f0d99ffce35b39e6805a7cac2d9faef4462100
LathErr0r <mathieu.niord@gmail.com>
2023-12-20 01:56:37 +0100

    Added commands state handling on output

--- src/terminal/menu.ml
+++ src/terminal/menu.ml
@@ -11,12 +11,11 @@ sig
   val lst_menu : t ref
   val set_menu : t -> unit
   val get_menu : unit -> t
-  val get_menu_title : t -> string          (* Returns the title of the menu *)
-  val get_commands : t -> Cmd.t list        (* Returns the list of options in the menu *)
-  val get_command : t -> int -> Cmd.t       (* Returns the option at the given index in the menu *)
-  val get_commands_length : t -> int        (* Returns the number of options in the menu *)
-  val get_commands_names : t -> string list (* Returns the names of the options in the menu *)
-  val run_command : t -> int -> unit        (* Runs the function associated with the option at the given index in the menu *)
+  val get_menu_title : t -> string    (* Returns the title of the menu *)
+  val get_commands : t -> Cmd.t list  (* Returns the list of options in the menu *)
+  val get_command : t -> int -> Cmd.t (* Returns the option at the given index in the menu *)
+  val get_commands_length : t -> int  (* Returns the number of options in the menu *)
+  val run_command : t -> int -> unit  (* Runs the function associated with the option at the given index in the menu *)
 end
 
 module Private : Private =
@@ -24,29 +23,27 @@ struct
   type t = string * Cmd.t list ;;
   let lst_menu : t ref = ref ("", []) ;;
 
-  let set_menu (menu : t) : unit = lst_menu := menu ;;
-  let get_menu () : t = !lst_menu ;;
+  let set_menu (menu : t) : unit = lst_menu := menu
+  let get_menu () : t = !lst_menu
 
-  let get_menu_title (m : t) : string = let (title, _) = m in title ;;
-  let get_commands (m : t) : Cmd.t list =  let (_, commands) = m in commands ;;
+  let get_menu_title (m : t) : string = let (title, _) = m in title
+  let get_commands (m : t) : Cmd.t list =  let (_, commands) = m in commands
 
   let get_command (m : t) (i : int) : Cmd.t =
     let commands = get_commands m in
-    List.nth commands (i - 1) ;;
+    List.nth commands (i - 1)
 
   let get_commands_length (m : t) : int =
     let commands = get_commands m in
-    List.length commands ;;
-
-  let get_commands_names (m : t) : string list =
-    let commands = get_commands m in
-    List.map (Cmd.get_name) commands ;;
+    List.length commands
 
   let run_command (menu : t) (selected : int) : unit =
+    let open Cmd in
     Display.make_action Clear ;
     let commands = get_commands menu in
     let cmd = List.nth commands (selected - 1) in
-    Cmd.run cmd ;;
+      if !(cmd.state) = IMPORTANT then set_state cmd ENABLED ;
+      run cmd
 end
 
 type t = Private.t
@@ -61,15 +58,16 @@ let show (menu : t) : unit =
   let title : string = get_menu_title menu
   and options : Cmd.t list = get_commands menu in
   let rec aux (choice : int) : unit =
-    let display, cols = build_options (get_commands_names menu) choice in (
+    let display, cols = build_options (get_commands menu) choice in (
       Display.print_output ((Output.build_title ()) ^ (Output.build_light_title title) ^ display);
       let input : Input.t = Input.get () in (
         let new_choice : int = Input.process input (List.length options) choice cols in (* Computing the new choice *)
-          if (input = ENTER) then run_command menu choice (* Running the selected option *)
-          else if (input = QUIT) then ()                  (* Otherwise, displaying the updated menu *)
+          if (input = ENTER) then
+            if (!((get_command menu choice).state) = Cmd.DISABLED) then aux choice
+            else run_command menu choice  (* Running the selected option *)
+          else if (input = QUIT) then ()  (* Otherwise, displaying the updated menu *)
           else aux new_choice
       )) in aux 1
-;;
 
 let back_to_menu () : unit =
   let open Private in
@@ -80,4 +78,3 @@ let back_to_menu () : unit =
       | ENTER -> show (get_menu ())
       | _ -> aux () in
     aux ()
-;;
--- src/terminal/output.ml
+++ src/terminal/output.ml
@@ -1,6 +1,10 @@
 
 (* Enum class of terminal colors *)
-type color = Black | Red | Green | Yellow | Blue | Magenta | Cyan | White | None
+type color = Black | Red | Green | Yellow | Blue | Magenta | Cyan | White | Grey | None
+
+let title   : string = SysUtils.read_file Config.title_path
+let credits : string = SysUtils.read_file Config.credits_path
+let flag_colors : color list = [Red; Yellow; Green; Cyan; Blue; Magenta]
 
 (* Parses color and returns terminal output code *)
 let code_of_color (c : color) (is_background : bool) : string =
@@ -14,20 +18,21 @@ let code_of_color (c : color) (is_background : bool) : string =
   | Magenta -> string_of_int (base_code + 5)
   | Cyan    -> string_of_int (base_code + 6)
   | White   -> string_of_int (base_code + 7)
+  | Grey    -> string_of_int (base_code + 60)
   | None    -> if is_background then "40" else "37"
 
 (* Colorize text with a color and background color *)
 let colorize (text : string) (c : color) (bg : color) : string =
-  let c = code_of_color c false in
-  let bg = code_of_color bg true in
+  let c   : string = code_of_color c false in
+  let bg  : string = code_of_color bg true in
   ("\027[" ^ c ^ ";" ^ bg ^ "m" ^ text ^ "\027[0m")
 
 (* Centers text on the terminal output *)
 let centered (text : string) : string =
-  let width : int = Terminal_width.get () in
-  let text_length = String.length text in
-  let spaces = String.make ((width - text_length) / 2) ' ' in
-  (spaces ^ text)
+  let width       : int     = Terminal_width.get ()
+  and text_length : int     = String.length text in
+  let spaces      : string  = String.make ((width - text_length) / 2) ' '
+  in (spaces ^ text)
 
 (* Get the maximum characters from lines based in the fixed number on columns
    Example:
@@ -35,8 +40,7 @@ let centered (text : string) : string =
       1 -> "fisrt".len + "second".len = 11
       2 -> "third".len = 5
       3 -> res = 11
-   Goal: Knowing a static max width for options display
-*)
+   Goal: Knowing a static max width for options display *)
 let max_line_len (options : string list) (columns : int) : int =
   let rec aux (options : string list) (max : int) (acc : int) (i : int) : int =
     match options with
@@ -45,37 +49,48 @@ let max_line_len (options : string list) (columns : int) : int =
       let len : int = String.length h in
       let new_acc : int = acc + len in
       if ((i + 1) mod columns = 0) then
-        let new_max : int = if new_acc > max then new_acc else max in
-        aux t new_max 0 (i + 1)
-      else
-        aux t max (acc + len) (i + 1)
+        let new_max : int =
+          if new_acc > max then new_acc else max
+        in aux t new_max 0 (i + 1)
+      else aux t max (acc + len) (i + 1)
   in aux options 0 0 0
 
 (* Defines the max word length *)
 let max_word_len (options : string list) : int =
-  let rec aux (options : string list) (max : int) : int =
-    match options with
-    | [] -> max
-    | h::t -> let len : int = String.length h in
-      if len > max then aux t len
-      else aux t max
-  in aux options 0
+  options |> List.map String.length |> List.fold_left max 0
+
+let option_output (cmd : Cmd.t) (number : int) (selected : bool) : string =
+  let open Cmd in
+  let fg_color : color = if selected then Black else
+    match !(cmd.state) with
+    | IMPORTANT -> Green
+    | ENABLED  -> White
+    | DISABLED -> Grey
+  and bg_color : color =
+    if selected then
+      match !(cmd.state) with
+      | IMPORTANT -> Green
+      | DISABLED -> Grey
+      | _ -> White
+    else None
+  in (colorize (string_of_int number ^ ". " ^ cmd.name) fg_color bg_color)
 
 (* Display a list of options on two columns from a list as:
    ["first"; "second"; "third"] -> "1. first\t\t2. second\n3. third\n"
-   The choice parameter defines the options that's going to be highlighted
-*)
-let build_options (options : string list) (choice : int) : string * int =
-  let width : int = !Display.columns             (* Width of the console *)
-  and max_word_len : int = (max_word_len options) in  (* Max length of a word + 4 spaces *)
-  let (max_chars, cols) : (int * int) =               (* Max length of a line *)
-    let max = max_line_len options 2
-    and min = max_line_len options 1 in
-      if (max + 10) < width then (max + 10), 2        (* 10 equals 3*2 ("1. ", "2. ") + 4 spaces between columns *)
+   The choice parameter defines the options that's going to be highlighted *)
+let build_options (options : Cmd.t list) (choice : int) : string * int =
+  let open Cmd in
+  let width : int = Terminal_width.get ()
+  and names : string list = List.map (fun c -> c.name) options in
+  let max_word_len : int = (max_word_len names) in
+  let (max_chars, cols) : (int * int) =
+    let max = max_line_len names 2
+    and min = max_line_len names 1 in
+      if (max + 10) < width then (max + 10), 2  (* 10 equals 3*2 ("1. ", "2. ") + 4 spaces between columns *)
       else (min + 3), 1
   in
   let begin_spaces : string = String.make ((width - max_chars) / 2) ' ' in (
-    let rec aux (options : string list) (choice : int) (i : int) (lst_wrd_len : int) : string =
+    let rec aux (options : Cmd.t list) (choice : int) (i : int) (lst_wrd_len : int) : string =
       match options with
       | [] -> ""
       | h :: t ->
@@ -87,11 +102,8 @@ let build_options (options : string list) (choice : int) : string * int =
             if i mod cols = 0 then spaces
             else ("\n\n" ^ begin_spaces)
         in
-        let option_str =
-          let opt : string = string_of_int i ^ ". " ^ h in
-          if i = choice then (colorize opt Black White)
-          else opt
-        in prefix ^ option_str ^ (aux t choice (i + 1) (String.length h))
+        let option_str = option_output h i (i = choice) in
+          prefix ^ option_str ^ (aux t choice (i + 1) (String.length h.name))
     in ((aux options choice 1 0) ^ "\n\n"), cols
   )
 
@@ -104,43 +116,20 @@ let build_options (options : string list) (choice : int) : string * int =
 let build_light_title (title : string) : string =
   let title_length = String.length title in
   let line = String.make (title_length + 10) '=' in
-  (centered (line ^ "\n"))
-  ^ (centered ("|    " ^ title ^ "    |\n"))
-  ^ (centered (line ^ "\n"))
+  "\n" ^ (centered line) ^ "\n"
+  ^ (centered ("|    " ^ title ^ "    |")) ^ "\n"
+  ^ (centered line) ^ "\n"
 
 (* Returns true if there's any space to contain the title in prompt *)
-let necessary_width () : bool =
-  let width = !Display.columns in
-  let title = open_in Config.title_path in
-    let enough : bool =
-      try let line = input_line title in String.length line < width
-      with End_of_file -> true
-    in (
-      close_in title;
-      enough
-    )
-
-let flag_colors : color list = [Red; Yellow; Green; Cyan; Blue; Magenta]
+let necessary_width () : bool = (Terminal_width.get ()) |> (<) (title |> Display.compute_width)
 
 (* Displays title with pride flag colors
-   Each line is print in one color of the pride flag
-   Colors list is [None; None; Red; Yellow; Green; Cyan; Blue; Magenta; Red; Yellow; Green; None; None;]
-*)
+   Each line is print in one color of the pride flag *)
 let build_title () : string =
-  if (necessary_width ()) then (
-    let title = open_in Config.title_path in
-    let rec write_title (colors : color list) acc =
-      match colors with
-      | [] -> failwith "Error: flag_colors list is empty"
-      | h :: t ->
-          try
-            let line = input_line title in
-            let colored_line = colorize (centered (line ^ "\n")) h None in
-            write_title (t @ [h]) (acc ^ colored_line)
-          with End_of_file -> acc
-    in 
-    let result = write_title flag_colors "" in
-    close_in title;
-    result
-  )
+  if (necessary_width ()) then
+    String.split_on_char '\n' title
+    |> List.mapi (fun i line ->
+      let color : color = List.nth flag_colors (i mod (List.length flag_colors)) in
+      (colorize (centered line) color None))
+    |> String.concat "\n"
   else centered "Welcome to VpkMl64!" ^ "\n\n"
